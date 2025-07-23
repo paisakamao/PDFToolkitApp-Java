d832e11ea029f356babc2ac0bf124fefa2ade33a
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-23 23:00:19 +0500

    Create MergePdfActivity.java

---
+++ app/src/main/java/com/pdf/toolkit/MergePdfActivity.java
@@ -0,0 +1,26 @@
+package com.pdf.toolkit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MergePdfActivity extends AppCompatActivity {
+
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merge_pdf);
+
+        // Initialize AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        // TODO: Implement merging logic
+    }
+}
