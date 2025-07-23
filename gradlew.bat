a5fd318b1521646516801fd5142b9c7f36c54939
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-23 16:47:33 +0500

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,14 @@
 
+@echo off
+set DIR=%~dp0
+set APP_HOME=%DIR%
+
+set DEFAULT_JVM_OPTS=
+
+if defined JAVA_HOME (
+    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+    set JAVA_EXE=java
+)
+
+"%JAVA_EXE%" %JAVA_OPTS% %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
