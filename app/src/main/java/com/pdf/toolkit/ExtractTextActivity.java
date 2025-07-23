0006d168b82f998d8b736d81034a8c6bda7cc66d
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-23 23:21:16 +0500

    Create ExtractTextActivity.java

---
+++ app/src/main/java/com/pdf/toolkit/ExtractTextActivity.java
@@ -0,0 +1,28 @@
+package com.pdf.toolkit;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class ExtractTextActivity extends AppCompatActivity {
+
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extract_text);
+
+        // Initialize AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        // Placeholder
+        Toast.makeText(this, "Extract Text Tool Opened", Toast.LENGTH_SHORT).show();
+    }
+}
