828c979d3871941e8ef2ec4d3327f8712b628a57
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-23 23:11:54 +0500

    Create ImageToPdfActivity.java

---
+++ app/src/main/java/com/pdf/toolkit/ImageToPdfActivity.java
@@ -0,0 +1,28 @@
+package com.pdf.toolkit;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class ImageToPdfActivity extends AppCompatActivity {
+
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_to_pdf);
+
+        // Initialize AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        // Placeholder Toast
+        Toast.makeText(this, "Image to PDF Tool Opened", Toast.LENGTH_SHORT).show();
+    }
+}
