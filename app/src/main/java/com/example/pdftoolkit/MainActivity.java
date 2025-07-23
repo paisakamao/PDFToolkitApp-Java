75f38de852d933ff2e64f587fd775f6da9f1263b
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-23 20:05:13 +0500

    Create MainActivity.java

---
+++ app/src/main/java/com/example/pdftoolkit/MainActivity.java
@@ -0,0 +1,12 @@
+package com.pdf.toolkit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
