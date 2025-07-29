86dae111f85543eb9210c019aafd050f24f9e9b1
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-29 17:52:59 +0500

    Update HomeActivity.java

--- app/src/main/java/com/pdf/toolkit/HomeActivity.java
+++ app/src/main/java/com/pdf/toolkit/HomeActivity.java
@@ -1,53 +1,52 @@
+please update it with new code
 package com.pdf.toolkit;
-
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-
+import com.pdf.toolkit.AllFilesActivity;
 public class HomeActivity extends AppCompatActivity {
+Generated code
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_home);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+Generated code
+CardView pdfToolCard = findViewById(R.id.card_pdf_tool);
+CardView scannerCard = findViewById(R.id.card_scanner);
+CardView allFilesCard = findViewById(R.id.card_all_files);
+CardView fileManagerCard = findViewById(R.id.card_file_manager);
+CardView uniToolsCard = findViewById(R.id.card_uni_tools);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("All Document");
-        }
+pdfToolCard.setOnClickListener(v -> launchWebViewActivity("index.html"));
 
-        CardView pdfToolCard = findViewById(R.id.card_pdf_tool);
-        CardView scannerCard = findViewById(R.id.card_scanner);
-        CardView allFilesCard = findViewById(R.id.card_all_files);
-        CardView fileManagerCard = findViewById(R.id.card_file_manager); // This is now the "Recent Files" button
-        CardView uniToolsCard = findViewById(R.id.card_uni_tools);
+scannerCard.setOnClickListener(v -> {
+    Intent intent = new Intent(HomeActivity.this, ScannerActivity.class);
+    startActivity(intent);
+});
 
-        pdfToolCard.setOnClickListener(v -> launchWebViewActivity("index.html"));
-        uniToolsCard.setOnClickListener(v -> launchWebViewActivity("unitools.html"));
+uniToolsCard.setOnClickListener(v -> launchWebViewActivity("unitools.html"));
 
-        allFilesCard.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, AllFilesActivity.class);
-            startActivity(intent);
-        });
+allFilesCard.setOnClickListener(v -> {
+    Intent intent = new Intent(HomeActivity.this, AllFilesActivity.class);
+    startActivity(intent);
+});
 
-        scannerCard.setOnClickListener(v -> {
-            // This assumes you will create a ScannerActivity.java file
-            Intent intent = new Intent(HomeActivity.this, ScannerActivity.class);
-            startActivity(intent);
-        });
-        
-        // --- START: THIS IS THE UPDATED LISTENER ---
-        // The "File Manager" card now opens the "Recent Files" screen.
-        fileManagerCard.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, RecentFilesActivity.class);
-            startActivity(intent);
-        });
-        // --- END: THIS IS THE UPDATED LISTENER ---
-    }
-
-    private void launchWebViewActivity(String fileName) {
-        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-        intent.putExtra(MainActivity.EXTRA_HTML_FILE, fileName);
+fileManagerCard.setOnClickListener(v -> {
+    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+    if (intent.resolveActivity(getPackageManager()) != null) {
         startActivity(intent);
     }
+});
+
+
+}
+
+private void launchWebViewActivity(String fileName) {
+Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+intent.putExtra("EXTRA_HTML_FILE", fileName);
+startActivity(intent);
+}
+
 }
