08de814076c2dd60932df4cd1cfe92380f3f9ad5
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-30 14:31:50 +0500

    Update HomeActivity.java

--- app/src/main/java/com/pdf/toolkit/HomeActivity.java
+++ app/src/main/java/com/pdf/toolkit/HomeActivity.java
@@ -8,39 +8,46 @@ import com.pdf.toolkit.AllFilesActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-    CardView pdfToolCard = findViewById(R.id.card_pdf_tool);
-    CardView scannerCard = findViewById(R.id.card_scanner);
-    CardView allFilesCard = findViewById(R.id.card_all_files);
-    CardView fileManagerCard = findViewById(R.id.card_file_manager);
-    CardView uniToolsCard = findViewById(R.id.card_uni_tools);
+        CardView pdfToolCard = findViewById(R.id.card_pdf_tool);
+        CardView scannerCard = findViewById(R.id.card_scanner);
+        CardView allFilesCard = findViewById(R.id.card_all_files);
+        CardView fileManagerCard = findViewById(R.id.card_file_manager);
+        CardView uniToolsCard = findViewById(R.id.card_uni_tools);
 
-    pdfToolCard.setOnClickListener(v -> launchWebViewActivity("index.html"));
-    scannerCard.setOnClickListener(v -> launchWebViewActivity("scanner.html"));
-    uniToolsCard.setOnClickListener(v -> launchWebViewActivity("unitools.html"));
-    
-    allFilesCard.setOnClickListener(v -> {
-        Intent intent = new Intent(HomeActivity.this, AllFilesActivity.class);
-        startActivity(intent);
-    });
-    
-    fileManagerCard.setOnClickListener(v -> {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        pdfToolCard.setOnClickListener(v -> launchWebViewActivity("index.html"));
+        uniToolsCard.setOnClickListener(v -> launchWebViewActivity("unitools.html"));
+
+        // --- THIS IS THE CORRECTED CODE FOR THE SCANNER CARD ---
+        scannerCard.setOnClickListener(v -> {
+            // We create an Intent to start our native ScannerActivity
+            Intent intent = new Intent(HomeActivity.this, ScannerActivity.class);
             startActivity(intent);
-        }
-    });
-}
+        });
+        // --- END OF CORRECTION ---
 
-private void launchWebViewActivity(String fileName) {
-    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-    intent.putExtra(MainActivity.EXTRA_HTML_FILE, fileName);
-    startActivity(intent);
-}
+        allFilesCard.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, AllFilesActivity.class);
+            startActivity(intent);
+        });
 
+        // The file manager card seems to be for picking a folder, which is different.
+        // Let's assume this is correct for now.
+        fileManagerCard.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        });
+    }
 
+    private void launchWebViewActivity(String fileName) {
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        intent.putExtra(MainActivity.EXTRA_HTML_FILE, fileName);
+        startActivity(intent);
+    }
 }
