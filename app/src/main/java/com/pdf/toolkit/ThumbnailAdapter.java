cf178991576346fb8e596c2dfb1f625f8b41bed6
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-07-31 13:48:44 +0500

    Update ThumbnailAdapter.java

--- app/src/main/java/com/pdf/toolkit/ThumbnailAdapter.java
+++ app/src/main/java/com/pdf/toolkit/ThumbnailAdapter.java
@@ -8,50 +8,50 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.io.File;
 import java.util.ArrayList;
 
 public class ThumbnailAdapter extends RecyclerView.Adapter<ThumbnailAdapter.ViewHolder> {
 
-    private final Context context;
-    private final ArrayList<Uri> imageUris;
+    private final ArrayList<String> imagePaths; // Now uses paths
     private final OnThumbnailListener listener;
 
     public interface OnThumbnailListener {
         void onThumbnailClick(int position);
     }
 
-    public ThumbnailAdapter(Context context, ArrayList<Uri> imageUris, OnThumbnailListener listener) {
-        this.context = context;
-        this.imageUris = imageUris;
+    public ThumbnailAdapter(ArrayList<String> imagePaths, OnThumbnailListener listener) {
+        this.imagePaths = imagePaths;
         this.listener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_thumbnail, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_thumbnail, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.thumbnailImage.setImageURI(imageUris.get(position));
+        File imgFile = new File(imagePaths.get(position));
+        if(imgFile.exists()){
+            holder.thumbnailImage.setImageURI(Uri.fromFile(imgFile));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return imageUris.size();
+        return imagePaths.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView thumbnailImage;
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             thumbnailImage = itemView.findViewById(R.id.thumbnail_image);
             itemView.setOnClickListener(this);
         }
-
         @Override
         public void onClick(View v) {
             listener.onThumbnailClick(getAdapterPosition());
