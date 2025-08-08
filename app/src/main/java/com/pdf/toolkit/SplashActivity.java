20eebffdd6a5eb007eea0610c50eeb988f61f479
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-08 23:36:27 +0500

    Update SplashActivity.java

--- app/src/main/java/com/pdf/toolkit/SplashActivity.java
+++ app/src/main/java/com/pdf/toolkit/SplashActivity.java
@@ -1,54 +1,32 @@
 package com.pdf.toolkit;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 5000; // 5 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Fullscreen only for splash
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(false);
-            WindowInsetsController controller = getWindow().getInsetsController();
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-                controller.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                );
-            }
-        } else {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            );
-        }
-
-        // Load GIF with Glide
-        ImageView gifView = findViewById(R.id.splash_gif);
+        // Load GIF
+        ImageView gifImageView = findViewById(R.id.splashGif);
         Glide.with(this)
                 .asGif()
                 .load(R.raw.my_splash)
-                .into(gifView);
+                .into(gifImageView);
 
-        // Wait 5 seconds then start main activity
+        // Delay to next activity
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 5000);
+        }, SPLASH_DURATION);
     }
 }
